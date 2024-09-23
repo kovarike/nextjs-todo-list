07636b31abda0bc2b503455bbf8f0b4bfad76f9c
kovarike <danilokovarike@gmail.com>
2024-09-23 15:50:25 -0300

    update#9

--- src/components/homePage.tsx
+++ src/components/homePage.tsx
@@ -32,8 +32,8 @@ export function HomePage() {
       <Header />
 
       <Dialog.Root>
-        <main className="flex flex-col items-center justify-center w-full">
-          <div className="w-full max-w-[450px] sm:max-w-[480px] px-3 py-2 mx-auto border-2 border-gray-500/50 bg-white rounded-xl space-y-4 flex flex-col justify-center items-center" style={{ height: 'calc(100vh - 200px)' }}>
+        <main className="flex flex-col items-center gap-2 w-full">
+          <div className="w-full max-w-[450px] sm:max-w-[480px] px-3 py-2 mx-auto border-2 border-gray-500/50 bg-white rounded-xl space-y-4 flex flex-col justify-center items-center" style={{ height: 'calc(100vh - 250px)' }}>
             <h1 className='text-center mb-3 text-gray-500 text-lg sm:text-base'>Suas tarefas de hoje</h1>
             <ul className='space-y-3 w-full overflow-y-scroll max-h-[450px] h-[450px]  mx-auto'>
               {data.map((item) => (
@@ -92,11 +92,14 @@ export function HomePage() {
                 </div>
               ))}
             </ul>
+          </div>
+          <div className="w-full max-w-[450px] sm:max-w-[480px]  p-1 mx-auto bg-transparent flex flex-col justify-center items-center" >
             <Dialog.Trigger asChild>
               <button className="bg-button border-none text-white py-3 px-5 rounded-md cursor-pointer text-center mx-auto w-full mt-5">
                 Adicionar nova tarefa
               </button>
             </Dialog.Trigger>
+            
           </div>
         </main>
         <Modal />
