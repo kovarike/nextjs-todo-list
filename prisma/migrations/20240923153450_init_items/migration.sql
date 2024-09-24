7d3f5f2a418401b37a2f825b51451e867eaab66e
kovarike <danilokovarike@gmail.com>
2024-09-24 10:08:53 -0300

    update#11

--- prisma/migrations/20240923153450_init_items/migration.sql
+++ prisma/migrations/20240923153450_init_items/migration.sql
@@ -9,3 +9,16 @@ CREATE TABLE "Item" (
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Item_id_key" ON "Item"("id");
+
+-- Habilitar RLS na tabela Item
+ALTER TABLE "Item" ENABLE ROW LEVEL SECURITY;
+
+-- Criar política de seleção
+CREATE POLICY select_policy ON "Item"
+FOR SELECT
+USING (true); -- Todos os usuários podem fazer SELECT
+
+-- Criar política para inserção, atualização e deleção apenas para administradores
+CREATE POLICY admin_policy ON "Item"
+FOR ALL
+USING (current_user = 'admin'); -- Altere 'admin' conforme necessário
